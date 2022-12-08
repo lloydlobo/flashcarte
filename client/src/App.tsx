32223d06aa7c47dd51ce92e58bf8cbb31d074ccc
Lloyd Lobo <76430758+lloydlobo@users.noreply.github.com>
2022-12-08 16:53:08 +0530

    feat(client): add react query to fetchMany decks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,87 +1,137 @@
 import {
-  ActionIcon, Anchor, AppShell, Autocomplete, Burger, Button, Card, Center, CloseButton, Container, createStyles, Flex, Footer,
-  Group, Header, Image, MantineProvider, MantineTheme, MediaQuery, Navbar, NavLink, SimpleGrid, Space,
-  Text, TextInput, Title, useMantineTheme
+  ActionIcon, Anchor, AppShell, Autocomplete, Burger, Button, Card, CloseButton, ColorScheme, ColorSchemeProvider, Container, createStyles, Flex, Footer, Group, Header, Image, MantineProvider, MediaQuery, Navbar, NavLink, SimpleGrid, Space, Text, TextInput, Title, useMantineColorScheme, useMantineTheme
 } from '@mantine/core'; // prettier-ignore
 import { useDisclosure } from '@mantine/hooks';
 import {
-  IconBrandInstagram,
-  IconBrandTwitter,
-  IconBrandYoutube,
-  IconCards,
-  IconSearch,
-  IconStack2,
-} from '@tabler/icons';
+  IconBrandInstagram, IconBrandTwitter, IconBrandYoutube, IconCards, IconMoonStars, IconSearch, IconStack2, IconSun
+} from '@tabler/icons'; // prettier-ignore
+import {
+  QueryClient,
+  QueryClientProvider,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ChangeEvent, Dispatch, FormEvent, ReactNode, useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  const [title, setTitle] = useState('');
-  const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
+export async function fetchDecks() {
+  const data = await fetch(`http://localhost:8080/api/decks`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
 
+  return { decks: data.decks };
+}
+
+export async function deleteDeck() {
+  throw new Error('deleteDeck not implemented.');
+}
+
+// Create a client
+const queryClient = new QueryClient();
+
+function App() {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   // prettier-ignore
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+          <AppLayoutShell />
+        </MantineProvider>
+      </ColorSchemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export function AppLayoutShell() {
+  const [title, setTitle] = useState('');
+
+  /**  Access the client. */
+  const queryClient = useQueryClient();
+  /** Queries. */
+  const query = useQuery({
+    queryKey: ['decks'],
+    queryFn: fetchDecks,
+  });
+  /** Mutations. */
+  const mutation = useMutation({
+    mutationFn: deleteDeck,
+    onSuccess: () => {
+      /** Invalidate and refetch. */
+      queryClient.invalidateQueries({ queryKey: ['decks'] });
+    },
+  });
+
   async function handleCreateDeck(e: FormEvent<HTMLFormElement>) {
     e.preventDefault(); // Tell HTML to avoid clearing the form.
-
     const updated = await fetch('http://localhost:8080/api/decks', {
-      method: 'POST', body: JSON.stringify({ title: title, }), headers: { 'Content-Type': 'application/json', },
-    }).then((res: Response) => res.json()).catch((err: any) => console.error('Failed to create deck.', err));
-
-    notify(`Created ${updated.deck.title}`)
+      method: 'POST',
+      body: JSON.stringify({ title: title }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res: Response) => res.json())
+      .catch((err: any) => console.error('Failed to create deck.', err));
+    notify(`Created ${updated.deck.title}`);
+    setTitle('');
   }
 
   return (
     <Layout>
       <section id="hero">
+        {/* prettier-ignore */}
         <Container mt={24}>
-          <Title hidden order={1}>
-            {BRAND.name}
-          </Title>
+          <Title hidden order={1}>{BRAND.name}</Title>
 
           <form action="submit" onSubmit={(e) => handleCreateDeck(e)}>
             {/* clicking on label with A11Y htmlFor + id allows users to click on label and auto-focus the input. */}
-            <Flex
-              align={'end'}
-              gap={'sm'}
-              direction={{ base: 'column', md: 'row' }}
-            >
-              <TextInput
-                label="Deck Title"
+            <Flex align={'end'} gap={'sm'} direction={{ base: 'column', md: 'row' }}>
+              <TextInput label="Deck Title"
                 placeholder="Enter the title of your deck"
                 className="w-full text-start"
                 value={title}
                 onChange={(e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value) } // prettier-ignore
               />
-
-              <Button variant="outline" type="submit">
-                Create Deck
-              </Button>
+              <Button variant="outline" type="submit">Create Deck</Button>
             </Flex>
           </form>
+
         </Container>
       </section>
-
       <Space h="xl" />
-
       <section id="decks">
         <Container>
-          <SimpleGrid
-            cols={4}
-            spacing="lg"
-            breakpoints={[
-              { maxWidth: 980, cols: 3, spacing: 'md' },
-              { maxWidth: 755, cols: 2, spacing: 'sm' },
-              { maxWidth: 600, cols: 1, spacing: 'sm' },
-            ]}
-          >
-            <DeckCard title={title} />
-            <DeckCard title={title} />
-            <DeckCard title={title} />
-            <DeckCard title={title} />
-            <DeckCard title={title} />
-          </SimpleGrid>
+          {query.data?.decks.length ? (
+            <SimpleGrid
+              cols={4}
+              spacing="lg"
+              breakpoints={[
+                { maxWidth: 980, cols: 3, spacing: 'md' },
+                { maxWidth: 755, cols: 2, spacing: 'sm' },
+                { maxWidth: 600, cols: 1, spacing: 'sm' },
+              ]}
+            >
+              {query.data.decks.map(
+                (deck: { _id: any; title: string }, index: number) => (
+                  <DeckCard
+                    key={`deck__${deck._id}}__${index}`}
+                    title={deck.title}
+                    mutation={mutation}
+                  />
+                ),
+              )}
+            </SimpleGrid>
+          ) : (
+            <Text>No decks yet.</Text>
+          )}
         </Container>
       </section>
     </Layout>
@@ -90,7 +140,13 @@ function App() {
 
 export default App;
 
-export function DeckCard({ title }: { title: string }) {
+export function DeckCard({
+  title,
+  mutation,
+}: {
+  title: string;
+  mutation: any;
+}) {
   return (
     // <Paper withBorder p="md" radius="md">
     <Card
@@ -100,7 +156,7 @@ export function DeckCard({ title }: { title: string }) {
       href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
       target="_blank"
     >
-      <Card.Section>
+      <Card.Section p={4}>
         <Image
           src="https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
           height={80}
@@ -127,7 +183,7 @@ export function DeckCard({ title }: { title: string }) {
   );
 }
 
-const useStyles = createStyles((theme) => ({
+export const useStylesHeader = createStyles((theme) => ({
   header: {
     paddingLeft: theme.spacing.md,
     paddingRight: theme.spacing.md,
@@ -175,21 +231,23 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-interface HeaderSearchProps {
+export interface HeaderSearchProps {
   links: { link: string; label: string }[];
 }
 
 export function HeaderSearch({
   links,
   setOpened,
-  theme,
 }: {
   links: HeaderSearchProps['links'];
   setOpened: Dispatch<React.SetStateAction<boolean>>;
-  theme: MantineTheme;
 }) {
   const [opened, { toggle }] = useDisclosure(false);
-  const { classes } = useStyles();
+  const { classes } = useStylesHeader();
+  const theme = useMantineTheme();
+
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
 
   const items = links.map((link) => (
     <a
@@ -223,7 +281,6 @@ export function HeaderSearch({
           href={'/'}
           underline={false}
           sx={{ lineHeight: 1 }}
-          // onClick={(event) => event.preventDefault()}
           size="sm"
         >
           <Group align="center">
@@ -247,7 +304,6 @@ export function HeaderSearch({
         <Group ml={50} spacing={5} className={classes.links}>
           {items}
         </Group>
-
         <Autocomplete
           className={classes.search}
           placeholder="Search"
@@ -262,6 +318,16 @@ export function HeaderSearch({
             'Blitz.js',
           ]}
         />
+        <Group position="center" my={30}>
+          <ActionIcon
+            variant="subtle"
+            // color={dark ? 'yellow' : 'blue'}
+            onClick={() => toggleColorScheme()}
+            title="Toggle color scheme"
+          >
+            {dark ? <IconSun size={18} /> : <IconMoonStars size={18} />}
+          </ActionIcon>
+        </Group>
       </Group>
     </div>
   );
@@ -275,86 +341,52 @@ export function HeaderSearch({
  * @see https://mantine.dev/core/app-shell/#responsive-styles
  */
 export function Layout({ children }: { children: ReactNode }) {
-  const theme = useMantineTheme();
   const [opened, setOpened] = useState(false);
 
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{ colorScheme: 'dark' }}
-    >
-      {/* To place Navbar and Aside components above Header and Footer, set layout="alt" on AppShell. */}
-      <AppShell // https://mantine.dev/core/app-shell/#responsive-styles
-        padding="md"
-        navbarOffsetBreakpoint="sm"
-        layout="default"
-        // asideOffsetBreakpoint="sm"
-        // style={{ backgroundImage: 'url(/myGrayBackgroundImage.png)' }}
-        styles={(theme) => ({
-          main: {
+    // To place Navbar and Aside components above Header and Footer, set layout="alt" on AppShell.
+    <AppShell // https://mantine.dev/core/app-shell/#responsive-styles
+      padding="md"
+      navbarOffsetBreakpoint="sm"
+      layout="default" // asideOffsetBreakpoint="sm"
+      styles={(theme) => ({
+        main: {
             backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
           }, // prettier-ignore
-        })}
-        header={
-          <Header height={{ base: 50, md: 70 }} p="md">
-            <div className="item-center flex h-full">
-              <HeaderSearch
-                links={[{ label: 'Docs', link: '/docs' }]}
-                theme={theme}
-                setOpened={setOpened}
-              />
-            </div>
-            <ToastContainer
-              position="top-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="dark"
+      })}
+      header={
+        <Header height={{ base: 50, md: 70 }} p="md">
+          <div className="item-center flex h-full">
+            <HeaderSearch
+              links={[{ label: 'Docs', link: '/docs' }]}
+              setOpened={setOpened}
             />
-          </Header>
-        }
-        navbar={
-          <Navbar
-            p="xs"
-            hiddenBreakpoint="sm"
-            hidden={!opened}
-            width={{ sm: 150, lg: 250 }}
-            // className={opened ? '' : 'hidden'}
-          >
-            <NavLink
-              label="Decks"
-              icon={<IconStack2 size={16} stroke={1.5} />}
-            />
-            <NavLink
-              label="Cards"
-              icon={<IconCards size={16} stroke={1.5} />}
-            />
-          </Navbar>
-        }
-        // aside={
-        //   <MediaQuery className="hidden" smallerThan="sm" styles={{ display: 'none' }} >
-        //     <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-        //       <Text>Application sidebar</Text>
-        //     </Aside>
-        //   </MediaQuery>
-        // }
-        footer={
-          <Footer p="md" height={{ base: '90', sm: '70' }}>
-            <FooterCentered
-              links={[ { link: '#', label: 'Contact' }, { link: '#', label: 'Privacy' }, { link: '#', label: 'Blog' }, { link: '#', label: 'Store' }, { link: '#', label: 'Careers' }, ]} // prettier-ignore
-            />
-          </Footer>
-        }
-      >
-        {children}
-      </AppShell>
-    </MantineProvider>
+          </div>
+          {/* prettier-ignore */}
+          <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
+        </Header>
+      }
+      navbar={
+        <Navbar // p="xs"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 150, lg: 250 }} // className={opened ? '' : 'hidden'}
+        >
+          <NavLink label="Decks" icon={<IconStack2 size={16} stroke={1.5} />} />
+          <NavLink label="Cards" icon={<IconCards size={16} stroke={1.5} />} />
+        </Navbar>
+      }
+      // aside={ <MediaQuery className="hidden" smallerThan="sm" styles={{ display: 'none' }} > <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}> <Text>Application sidebar</Text> </Aside> </MediaQuery> }
+      footer={
+        <Footer p="md" height={{ base: '90', sm: '70' }}>
+          <FooterCentered
+            links={[ { link: '#', label: 'Contact' }, { link: '#', label: 'Privacy' }, { link: '#', label: 'Blog' }, { link: '#', label: 'Store' }, { link: '#', label: 'Careers' }, ]} // prettier-ignore
+          />
+        </Footer>
+      }
+    >
+      {children}
+    </AppShell>
   );
 }
 
@@ -362,7 +394,7 @@ export const BRAND = {
   name: 'flashcarte',
 };
 
-const notify = (content: string) =>
+export const notify = (content: string) =>
   toast(content, {
     position: 'top-right',
     autoClose: 2000,
@@ -374,7 +406,7 @@ const notify = (content: string) =>
     theme: 'dark',
   });
 
-const useStylesFooter = createStyles((theme) => ({
+export const useStylesFooter = createStyles((theme) => ({
   footer: {
     marginTop: 120,
     borderTop: `1px solid ${
@@ -403,7 +435,7 @@ const useStylesFooter = createStyles((theme) => ({
   },
 }));
 
-interface FooterCenteredProps {
+export interface FooterCenteredProps {
   links: { link: string; label: string }[];
 }
 
